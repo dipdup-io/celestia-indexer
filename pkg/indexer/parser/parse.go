9d919bcefdbc923387e6d98fba6b0b4203a9748a
Lavysh Alexander <LavyshAlexander@gmail.com>
2023-09-01 13:50:23 +0300

    Updated logic of receiver worker, refactored pipeline with blockData.

--- pkg/indexer/parser/parse.go
+++ pkg/indexer/parser/parse.go
@@ -4,39 +4,39 @@ import (
 	"context"
 	"github.com/dipdup-io/celestia-indexer/internal/storage"
 	storageTypes "github.com/dipdup-io/celestia-indexer/internal/storage/types"
-	"github.com/dipdup-io/celestia-indexer/pkg/node/types"
+	"github.com/dipdup-io/celestia-indexer/pkg/types"
 	"github.com/shopspring/decimal"
 	"strconv"
 )
 
-func (p *Parser) parse(ctx context.Context, resultBlock types.ResultBlock) error {
-	p.log.Info().Int64("height", resultBlock.Block.Height).Msg("parsing block...")
+func (p *Parser) parse(ctx context.Context, b types.BlockData) error {
+	p.log.Info().Int64("height", b.Block.Height).Msg("parsing block...")
 
 	block := storage.Block{
-		Height:       storage.Level(resultBlock.Block.Height),
-		Time:         resultBlock.Block.Time,
-		VersionBlock: strconv.FormatUint(resultBlock.Block.Version.Block, 10), // should we use string in storage type?
-		VersionApp:   strconv.FormatUint(resultBlock.Block.Version.App, 10),   // should we use string in storage type?
+		Height:       b.Height,
+		Time:         b.Block.Time,
+		VersionBlock: strconv.FormatUint(b.Block.Version.Block, 10), // should we use string in storage type?
+		VersionApp:   strconv.FormatUint(b.Block.Version.App, 10),   // should we use string in storage type?
 
-		TxCount:       uint64(len(resultBlock.Block.Data.Txs)),
+		TxCount:       uint64(len(b.Block.Data.Txs)),
 		EventsCount:   0, // TODO
 		MessageTypes:  storageTypes.MsgTypeBits{},
 		NamespaceSize: 0, // "Summary block namespace size from pay for blob"` // should it be in block?
 
-		Hash:               []byte(resultBlock.BlockID.Hash), // create a Hex type for common usage through indexer app
-		ParentHash:         []byte(resultBlock.Block.LastBlockID.Hash),
-		LastCommitHash:     []byte(resultBlock.Block.LastCommitHash),
-		DataHash:           []byte(resultBlock.Block.DataHash),
-		ValidatorsHash:     []byte(resultBlock.Block.ValidatorsHash),
-		NextValidatorsHash: []byte(resultBlock.Block.NextValidatorsHash),
-		ConsensusHash:      []byte(resultBlock.Block.ConsensusHash),
-		AppHash:            []byte(resultBlock.Block.AppHash),
-		LastResultsHash:    []byte(resultBlock.Block.LastResultsHash),
-		EvidenceHash:       []byte(resultBlock.Block.EvidenceHash),
-		ProposerAddress:    []byte(resultBlock.Block.ProposerAddress),
+		Hash:               []byte(b.BlockID.Hash), // create a Hex type for common usage through indexer app
+		ParentHash:         []byte(b.Block.LastBlockID.Hash),
+		LastCommitHash:     []byte(b.Block.LastCommitHash),
+		DataHash:           []byte(b.Block.DataHash),
+		ValidatorsHash:     []byte(b.Block.ValidatorsHash),
+		NextValidatorsHash: []byte(b.Block.NextValidatorsHash),
+		ConsensusHash:      []byte(b.Block.ConsensusHash),
+		AppHash:            []byte(b.Block.AppHash),
+		LastResultsHash:    []byte(b.Block.LastResultsHash),
+		EvidenceHash:       []byte(b.Block.EvidenceHash),
+		ProposerAddress:    []byte(b.Block.ProposerAddress),
 
 		Fee:     decimal.Zero, // TODO
-		ChainId: resultBlock.Block.ChainID,
+		ChainId: b.Block.ChainID,
 
 		Txs:    make([]storage.Tx, 0),    // TODO
 		Events: make([]storage.Event, 0), // TODO
