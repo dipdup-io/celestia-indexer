80c981b64e4cf48a9948e8c9ebef6fba8e1fe300
Lavysh Alexander <LavyshAlexander@gmail.com>
2023-09-06 19:03:03 +0300

    Added check of current receiver hash with block hash on chain.

--- pkg/indexer/receiver/readBlocks.go
+++ pkg/indexer/receiver/readBlocks.go
@@ -27,12 +27,10 @@ func (r *Receiver) readBlocks(ctx context.Context) error {
 		}
 	}
 
-	receiverLevel, receiverHash := r.Level()
-	if receiverLevel == headLevel && !bytes.Equal(receiverHash, headHash) {
-		r.log.Info().
-			Str("receiverHash", hex.EncodeToString(receiverHash)).
-			Str("headHash", hex.EncodeToString(headHash)).
-			Msg("rollback detected")
+	if rollbackDetected, err := r.checkRollback(ctx, headLevel, headHash); err != nil {
+		return errors.Wrap(err, "while detecting rollback")
+	} else if rollbackDetected {
+		r.log.Debug().Msg("rollback detected")
 		// TODO	call rollback to the rescue
 	}
 
@@ -47,4 +45,38 @@ func (r *Receiver) headLevel(ctx context.Context) (storage.Level, []byte, error)
 
 	headLevel := storage.Level(head.Block.Height)
 	return headLevel, head.BlockID.Hash, nil
+}
+
+func (r *Receiver) checkRollback(ctx context.Context, headLevel storage.Level, headHash []byte) (rollbackDetected bool, err error) {
+	receiverLevel, receiverHash := r.Level()
+
+	if receiverLevel == headLevel && !bytes.Equal(receiverHash, headHash) { // TODO-DISCUSS seems like it won't happen
+		r.log.Info().
+			Str("receiverHash", hex.EncodeToString(receiverHash)).
+			Str("headHash", hex.EncodeToString(headHash)).
+			Msg("rollback detected")
+
+		rollbackDetected = true
+		return
+	}
+
+	if receiverLevel != headLevel {
+		block, e := r.api.Block(ctx, receiverLevel)
+		if e != nil {
+			err = errors.Wrapf(e, "while getting block by receiver level=%d", receiverLevel)
+			return
+		}
+
+		if !bytes.Equal(receiverHash, block.BlockID.Hash) {
+			r.log.Info().
+				Str("receiverHash", hex.EncodeToString(receiverHash)).
+				Str("blockHash", hex.EncodeToString(block.BlockID.Hash)).
+				Msg("rollback detected")
+
+			rollbackDetected = true
+			return
+		}
+	}
+
+	return
 }
