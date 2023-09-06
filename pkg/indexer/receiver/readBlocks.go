0f0559d50283d6daecd4130482a889b90cf99e00
Lavysh Alexander <LavyshAlexander@gmail.com>
2023-09-06 13:45:06 +0300

    Added logic to catch more recent head.

--- pkg/indexer/receiver/readBlocks.go
+++ pkg/indexer/receiver/readBlocks.go
@@ -7,22 +7,38 @@ import (
 )
 
 func (r *Receiver) readBlocks(ctx context.Context) error {
-	head, err := r.api.Head(ctx)
+	headLevel, err := r.headLevel(ctx)
 	if err != nil {
 		return err
 	}
 
 	startLevel := storage.Level(r.cfg.Indexer.StartLevel) // TODO read from current state
-	headLevel := storage.Level(head.Block.Height)
+	level := startLevel
 
-	for level := startLevel; level <= headLevel; level++ {
-		select {
-		case <-ctx.Done():
-			return nil
-		default:
-			r.pool.AddTask(level)
+	for level <= headLevel {
+		for ; level <= headLevel; level++ {
+			select {
+			case <-ctx.Done():
+				return nil
+			default:
+				r.pool.AddTask(level)
+			}
+		}
+
+		if headLevel, err = r.headLevel(ctx); err != nil {
+			return err
 		}
 	}
 
 	return nil
+}
+
+func (r *Receiver) headLevel(ctx context.Context) (storage.Level, error) {
+	head, err := r.api.Head(ctx)
+	if err != nil {
+		return 0, err
+	}
+
+	headLevel := storage.Level(head.Block.Height)
+	return headLevel, nil
 }
